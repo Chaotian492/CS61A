cbd4f4032d69fe524de41a07df2f2b77b8a5eaa8
Chaotian492 <chaotian492@gmail.com>
2024-09-25 23:32:28 +0800

    scheme second commit

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -38,6 +38,11 @@ def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        # eval first by scheme
+        operator = scheme_eval(first, env)
+        validate_procedure(operator)
+        operands = rest.map(lambda expr: scheme_eval(expr, env))
+        return scheme_apply(operator, operands, env)
         # END PROBLEM 4
 
 
@@ -73,9 +78,14 @@ def eval_all(expressions, env):
     2
     """
     # BEGIN PROBLEM 7
-    return scheme_eval(
-        expressions.first, env
-    )  # replace this with lines of your own code
+    if expressions is nil:
+        return None
+    elif expressions.rest is nil:
+        return scheme_eval(expressions.first, env)
+    else:
+        scheme_eval(expressions.first, env)
+        return eval_all(expressions.rest, env)
+    # replace this with lines of your own code
     # END PROBLEM 7
 
 
@@ -177,6 +187,11 @@ class BuiltinProcedure(Procedure):
         python_args = []
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        while args is not nil:
+            python_args.append(args.first)
+            args = args.rest
+        if self.use_env is True:
+            python_args.append(env)
         # END PROBLEM 3
         try:
             return self.fn(*python_args)
@@ -269,6 +284,10 @@ def do_define_form(expressions, env):
         )  # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 5
         "*** YOUR CODE HERE ***"
+        # rest include expression and nil, we only need expression
+        value = scheme_eval(expressions.rest.first, env)
+        env.define(target, value)
+        return target
         # END PROBLEM 5
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
         # BEGIN PROBLEM 9
@@ -289,6 +308,7 @@ def do_quote_form(expressions, env):
     validate_form(expressions, 1, 1)
     # BEGIN PROBLEM 6
     "*** YOUR CODE HERE ***"
+    return expressions.first
     # END PROBLEM 6
 
 
